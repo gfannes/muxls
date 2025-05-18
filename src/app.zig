15cfa78fb92b4f9886e16445cec9bd973bc05178
Geert Fannes <geert.fannes@auro-3d.com>
2025-05-18 11:30:11 +0200

    WIP on loading config

--- src/app.zig
+++ src/app.zig
@@ -1,22 +1,39 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 const rubr = @import("rubr");
+
+const cfg = @import("cfg.zig");
 
 pub const App = struct {
     const Self = @This();
 
     log: rubr.log.Log = .{},
-    gpa: std.heap.GeneralPurposeAllocator(.{}) = .{},
 
+    gpa: std.heap.GeneralPurposeAllocator(.{}) = .{},
     a: std.mem.Allocator = undefined,
+
+    config: cfg.Config = .{},
+    config_loader: cfg.Loader = undefined,
+
+    server: rubr.lsp.Server = undefined,
+    do_continue: bool = true,
 
     pub fn init(self: *Self) !void {
         self.log.init();
         try self.log.toFile("/tmp/muxls.log");
 
         self.a = self.gpa.allocator();
+
+        self.config_loader = cfg.Loader.init(&self.config, self.a);
+
+        self.server = rubr.lsp.Server.init(std.io.getStdIn().reader(), std.io.getStdOut().writer(), self.log.writer(), self.a);
     }
     pub fn deinit(self: *Self) void {
+        self.server.deinit();
+
+        self.config_loader.deinit();
+
         if (self.gpa.deinit() == std.heap.Check.leak)
             self.log.warning("Found memory leaks\n", .{}) catch {};
 
@@ -25,6 +42,21 @@ pub const App = struct {
     }
 
     pub fn run(self: *Self) !void {
-        _ = self;
+        try self.loadConfig_();
+
+        var iteration: usize = 0;
+        while (self.do_continue) : (iteration += 1) {
+            try self.log.info("Iteration {}\n", .{iteration});
+
+            if (self.server.receive()) |_| {} else |_| {}
+        }
+    }
+
+    fn loadConfig_(self: *Self) !void {
+        // &todo: Replace hardcoded HOME folder
+        // &:zig:build:info Couple filename with build.zig.zon#name
+        const fp = if (builtin.os.tag == .macos) "/Users/geertf/.config/champ/config.zon" else "/home/geertf/.config/champ/config.zon";
+
+        try self.config_loader.loadFromFile(fp);
     }
 };
--- src/cfg.zig
+++ src/cfg.zig
@@ -1,5 +1,67 @@
 const std = @import("std");
 
+pub const Config = struct {
+    pub const Server = struct {
+        name: []const u8,
+        cmd: []const u8,
+        args: ?[][]const u8,
+    };
+    pub const Language = struct {
+        name: []const u8,
+        extensions: [][]const u8,
+        servers: [][]const u8,
+    };
+
+    servers: []Server = &.{},
+    languages: []Language = &.{},
+};
+
+pub const Loader = struct {
+    const Self = @This();
+
+    config: *Config,
+    aa: std.heap.ArenaAllocator = undefined,
+
+    pub fn init(config: *Config, a: std.mem.Allocator) Self {
+        return Self{ .config = config, .aa = std.heap.ArenaAllocator.init(a) };
+    }
+    pub fn deinit(self: *Self) void {
+        self.aa.deinit();
+    }
+
+    // For some reason, std.zon.parse.fromSlice() expects a sentinel string
+    pub fn loadFromContent(self: *Self, content: [:0]const u8) !void {
+        self.config.* = try std.zon.parse.fromSlice(Config, self.aa.allocator(), content, null, .{});
+
+        try self.normalize();
+    }
+
+    pub fn loadFromFile(self: *Self, filename: []const u8) !void {
+        var file = try std.fs.openFileAbsolute(filename, .{});
+        defer file.close();
+
+        // For some reason, std.zon.parse.fromSlice() expects a sentinel string
+        const content = try file.readToEndAllocOptions(self.aa.allocator(), std.math.maxInt(usize), null, 1, 0);
+
+        try self.loadFromContent(content);
+    }
+
+    // - Rework include extensions from 'md' to '.md'
+    fn normalize(self: *Self) !void {
+        const a = self.aa.allocator();
+        for (self.config.languages) |*language| {
+            const new_extensions = try a.alloc([]const u8, language.extensions.len);
+            for (language.extensions, 0..) |ext, ix| {
+                new_extensions[ix] = if (ext.len > 0 and ext[0] != '.')
+                    try std.mem.concat(a, u8, &[_][]const u8{ ".", ext })
+                else
+                    ext;
+            }
+            language.extensions = new_extensions;
+        }
+    }
+};
+
 test "cfg" {
     const ut = std.testing;
     try ut.expect(false);
